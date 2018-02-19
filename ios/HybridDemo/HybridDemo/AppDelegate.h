adae70794c915e4db4d9097eac2046ba8114fb5f
Szymon Ulewicz <ulewicz@amazon.com>
2018-02-18 18:53:00 -0800

    Empty project added.

---
+++ ios/HybridDemo/HybridDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HybridDemo
+//
+//  Created by Ulewicz, Szymon on 2/18/18.
+//  Copyright © 2018 Ulewicz, Szymon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/HybridDemo/HybridDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HybridDemo
+//
+//  Created by Ulewicz, Szymon on 2/18/18.
+//  Copyright © 2018 Ulewicz, Szymon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
